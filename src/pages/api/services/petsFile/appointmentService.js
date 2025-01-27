842740990c083c6eea355868508abd4a90e3b18d
Erick Vera <erickverac90@gmail.com>
2025-01-26 22:54:25 -0600

    Agregando servicios para peticiones de citas

---
+++ src/pages/api/services/petsFile/appointmentService.js
@@ -0,0 +1,89 @@
+const api = process.env.NEXT_PUBLIC_API_URL;
+
+export async function createAppointment(appointmentData) {
+  try {
+    const token = localStorage.getItem("access-token");
+    if (!token) {
+      throw new Error("No access token found");
+    }
+
+    const response = await fetch(`${api}/appointments`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(appointmentData),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      const errorMessage = data.message || "Error creating appointment.";
+      throw new Error(errorMessage);
+    }
+
+    return data.data.appointment;
+  } catch (error) {
+    console.error("Error createAppointment:", error.message);
+    throw error;
+  }
+}
+
+export async function updateAppointment(appointmentId, appointmentData) {
+  try {
+    const token = localStorage.getItem("access-token");
+    if (!token) {
+      throw new Error("No access token found");
+    }
+
+    const response = await fetch(`${api}/appointments/${appointmentId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(appointmentData),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      const errorMessage = data.message || "Error updating appointment.";
+      throw new Error(errorMessage);
+    }
+
+    return data.data.appointment;
+  } catch (error) {
+    console.error("Error updateAppointment:", error.message);
+    throw error;
+  }
+}
+
+export async function deleteAppointment(appointmentId) {
+  try {
+    const token = localStorage.getItem("access-token");
+    if (!token) {
+      throw new Error("No access token found");
+    }
+
+    const response = await fetch(`${api}/appointments/${appointmentId}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      const errorMessage = data.message || "Error deleting appointment.";
+      throw new Error(errorMessage);
+    }
+
+    return data.data.appointment;
+  } catch (error) {
+    console.error("Error deleteAppointment:", error.message);
+    throw error;
+  }
+}
