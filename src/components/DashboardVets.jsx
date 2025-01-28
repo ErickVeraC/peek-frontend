0b562e649bb36fd8cfb42c77e8a05af6ebdea329
Arturo Juarez <ajrobles98@gmail.com>
2025-01-27 20:11:27 -0600

    feat : adding pets to dashboard vet

--- src/components/DashboardVets.jsx
+++ src/components/DashboardVets.jsx
@@ -3,6 +3,8 @@ import SideBar from "@/components/SideBar";
 import { useState } from "react";
 import { useAccount } from "@/context/AccountContext";
 import Image from "next/image";
+import { TfiArrowCircleRight, TfiArrowCircleDown } from "react-icons/tfi";
+import PetDate from "./PetDate";
 
 export default function DashboardVets() {
   const { account } = useAccount();
@@ -10,41 +12,68 @@ export default function DashboardVets() {
     <main className="bg-gray-100 min-h-screen">
       <Header />
       <SideBar />
-      <div className="bg-gray-100 ml-20 p-8 grid gap-4 grid-cols-1 lg:grid-cols-12 ">
-        <div className="grid grid-cols-subgrid gap-4 md:grid-cols-2  lg:col-span-3 xl:col-span-2 lg:grid-cols-1 ">
-          <div className="h-44 lg:h-96 bg-white shadow-md rounded-2xl p-6 flex flex-col gap-4">
-            <img
-              src={account.profilePic}
-              alt=""
-              className=" max-h-full rounded-lg"
-            />
-            <div className="flex flex-row gap-2 justify-between items-center">
-              <div className="text-black flex flex-col ">
-                <h2 className="font-semibold text-lg">
-                  Dr. {account.name + account.lastName}
-                </h2>
-                <h2>Médico veterinario</h2>
-                <h2>CED. PROF. 1265689894</h2>
-              </div>
-              <div class=" size-6 bg-green-500 rounded-full"></div>
-            </div>
-          </div>
-          <div className="min-h-16 lg:h-80 bg-white shadow-md rounded-2xl p-6">
-            <h2 className="text-congress-950">Proximas citas</h2>
+      <div className="bg-gray-100 ml-20 p-8 grid gap-4 grid-cols-1 lg:grid-cols-12">
+        <div className="bg-white min-h-10 gap-4 shadow-lg rounded-2xl p-4 flex flex-row lg:flex-col lg:col-span-3 2xl:col-span-2 ">
+          <img
+            src={account.profilePic}
+            alt=""
+            className="rounded-lg max-h-52"
+          />
+          <div className="text-black flex flex-col w-full justify-start ">
+            <h2 className="font-semibold text-sm sm:text-2xl lg:text-2xl">
+              Dr. {account.name + account.lastName}
+            </h2>
+            <h2 className="text-xs sm:text-lg">Médico veterinario</h2>
+            <h2 className="text-sm sm:text-lg">CED. PROF. 1265689894</h2>
           </div>
         </div>
-        <div className="grid grid-cols-subgrid gap-4 md:grid-cols-2  lg:col-span-9 xl:col-span-10 lg:grid-cols-3 lg:content-center">
-          <div className="md:col-span-2 lg:col-span-3  lg:row-span-1 text-3xl font-urbanist max-w-md text-congress-950 mb-5">
-            <h2 className="text-orange-500">
-              <span>Te damos la Bienvenida</span>{" "}
-              <span className="text-congress-950">
-                ¡Comienza a administrar tu perfil!
-              </span>
-            </h2>
-            <div className="text-congress-950 flex gap-2 mt-5">
-              <span>Mis mascotas</span>
+        <div
+          name="citas"
+          className="flex flex-col gap-3 lg:flex-row lg:flex-wrap  lg:col-span-9 2xl:col-span-10 w-full"
+        >
+          <div className=" bg-white rounded-2xl shadow-lg flex flex-row gap-1 justify-center items-center lg:w-1/6 xl:w-1/6">
+            <div className="p-2 text-black flex flex-col items-center justify-center text-2xl gap-3 text-center">
+              <h2>Proximas citas</h2>
+              <TfiArrowCircleRight className="hidden lg:block" />
+              <TfiArrowCircleDown className="block lg:hidden" />
             </div>
           </div>
+          <PetDate></PetDate>
+          <PetDate></PetDate>
+          <PetDate></PetDate>
+          <PetDate></PetDate>
+        </div>
+      </div>
+      <div className="bg-gray-100 ml-20 p-8 pt-0 grid gap-4 grid-cols-1 lg:grid-cols-12">
+        <div className="bg-white min-h-10 gap-4 shadow-lg rounded-2xl p-4 flex flex-row lg:flex-col lg:col-span-3 2xl:col-span-2 ">
+          <img
+            src={account.profilePic}
+            alt=""
+            className="rounded-lg max-h-52"
+          />
+          <div className="text-black flex flex-col w-full justify-start ">
+            <h2 className="font-semibold text-sm sm:text-2xl lg:text-2xl">
+              Dr. {account.name + account.lastName}
+            </h2>
+            <h2 className="text-xs sm:text-lg">Médico veterinario</h2>
+            <h2 className="text-sm sm:text-lg">CED. PROF. 1265689894</h2>
+          </div>
+        </div>
+        <div
+          name="citas"
+          className="flex flex-col gap-3 lg:flex-row lg:flex-wrap  lg:col-span-9 2xl:col-span-10 w-full"
+        >
+          <div className=" bg-white rounded-2xl shadow-lg flex flex-row gap-1 justify-center items-center lg:w-1/6 xl:w-1/6">
+            <div className="p-2 text-black flex flex-col items-center justify-center text-2xl gap-3 text-center">
+              <h2>Proximas citas</h2>
+              <TfiArrowCircleRight className="hidden lg:block" />
+              <TfiArrowCircleDown className="block lg:hidden" />
+            </div>
+          </div>
+          <PetDate></PetDate>
+          <PetDate></PetDate>
+          <PetDate></PetDate>
+          <PetDate></PetDate>
         </div>
       </div>
     </main>
